fec9562819fdfbf7db2ab3a456382c64a6e04194
BernardoVieiraDev <bernardovdev@gmail.com>
2025-10-26 19:12:15 -0300

    amaas

--- dio_blog/src/controllers/auth.py
+++ dio_blog/src/controllers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from security import sign_jwt, TokenResponse
+from src.security import sign_jwt, TokenResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
